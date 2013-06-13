bd8e631e61c4e783aaf40ea9464e7c905c9c17ab
sagtrading <sagtradings@gmail.com>
2013-06-12 22:14:29 -0400

    add callbacks for onsubscribe / error

--- ConsoleApplication5/quotationeventhandler.cpp
+++ ConsoleApplication5/quotationeventhandler.cpp
@@ -225,18 +225,104 @@ class quotationeventhandler : public CThostFtdcMdSpi{
 		}
 
 
-		virtual void OnRspQryDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast){
-			//printf("You did it this time jerk");
-		}
-
 		virtual void OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast){
-			//printf("OnRspError:\n");
-			//printf("ErrorCode=[%d], ErrorMsg=[%s]\n", pRspInfo->ErrorID,pRspInfo->ErrorMsg);
-			//printf("RequestID=[%d], Chain=[%d]\n", nRequestID, bIsLast);
+			printf("OnRspError\n");
+			JNIEnv * g_env;
+			jint success = cachedJvm -> GetEnv((void**)&g_env, JNI_VERSION_1_6);
+			if(success != JNI_OK){
+			//	printf("attempting to resync jvm to thread\n");
+				jint attachSuccess = cachedJvm ->AttachCurrentThread((void**)&g_env, NULL);
+				if(attachSuccess == 0){
+					//printf("resync successful!\n");
+				}
+				else{
+					//printf("resync unsuccessful\n");
+					return;
+				}
+			}
+			list<jobject>::iterator it = observers.begin();			
+			while (it != observers.end()) {
+				
+				printf("getting subscribed object and onRspError method ID\n");
+				jobject &obj = *it;
+				jclass cls = g_env->GetObjectClass(obj);
+				jmethodID mid = g_env->GetMethodID(cls, "onRspError", "(Lbo/ErrorResult;)V");
+
+				printf("Creating Parameter Object ErrorResult\n");
+				jclass parameter = g_env->FindClass("bo/ErrorResult");
+			    jmethodID midConstructor = (g_env)->GetMethodID(parameter, "<init>", "()V");
+				jobject paramObject = (g_env)->NewObject(parameter, midConstructor);
+
+				printf("getting ID's 1 - 5\n");
+				jmethodID errorIDId = g_env->GetMethodID(parameter, "setErrorId", "(I)V");
+				jmethodID errorMessageId = g_env->GetMethodID(parameter, "setErrorMessage", "(Ljava/lang/String;)V");
+				
+				printf("invoking setters 1\n");
+				g_env->CallVoidMethod(paramObject, errorIDId, pRspInfo->ErrorID);
+				printf("invoking setter 2\n");
+				jstring j_errorMessage = g_env->NewStringUTF(pRspInfo->ErrorMsg);
+				g_env->CallVoidMethod(paramObject, errorMessageId, j_errorMessage);
+
+
+				if (mid == 0) {
+					//printf("mid was 0!!!\n");
+					return;
+				}
+
+				g_env->CallVoidMethod(obj, mid, paramObject);
+				it++;
+
+			}
 		}
 
 		virtual void OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {
-			//printf("SUBSCRIBBBED MARKET DATA!!!!!");
+			printf("OnRspSubMarketData\n");
+			JNIEnv * g_env;
+			jint success = cachedJvm -> GetEnv((void**)&g_env, JNI_VERSION_1_6);
+			if(success != JNI_OK){
+			//	printf("attempting to resync jvm to thread\n");
+				jint attachSuccess = cachedJvm ->AttachCurrentThread((void**)&g_env, NULL);
+				if(attachSuccess == 0){
+					//printf("resync successful!\n");
+				}
+				else{
+					//printf("resync unsuccessful\n");
+					return;
+				}
+			}
+			list<jobject>::iterator it = observers.begin();			
+			while (it != observers.end()) {
+				
+				
+				jobject &obj = *it;
+				
+				jclass cls = g_env->GetObjectClass(obj);
+				printf("getting callback ID for onSubscribeMarketDataResponse and param SubscribeMarketDataResponse\n");
+				jmethodID mid = g_env->GetMethodID(cls, "onSubscribeMarketDataResponse", "(Lbo/SubscribeMarketDataResponse;)V");
+
+				jclass parameter = g_env->FindClass("bo/SubscribeMarketDataResponse");
+			    jmethodID midConstructor = (g_env)->GetMethodID(parameter, "<init>", "()V");
+				jobject paramObject = (g_env)->NewObject(parameter, midConstructor);
+
+				printf("getting method ID's\n");
+				jmethodID errorIDId = g_env->GetMethodID(parameter, "setErrorId", "(I)V");
+				jmethodID errorMsgId = g_env->GetMethodID(parameter, "setErrorMsg", "(Ljava/lang/String;)V");
+				jmethodID specificInstrumentId = g_env->GetMethodID(parameter, "setSpecificInstrument", "(Ljava/lang/String;)V");
+
+				printf("invoking setters\n");
+
+				g_env->CallVoidMethod(paramObject, errorIDId, pRspInfo->ErrorID);
+				g_env->CallVoidMethod(paramObject, errorMsgId, g_env->NewStringUTF(pRspInfo->ErrorMsg));
+				g_env->CallVoidMethod(paramObject, specificInstrumentId, g_env->NewStringUTF(pSpecificInstrument->InstrumentID));
+				if (mid == 0) {
+					//printf("mid was 0!!!\n");
+					return;
+				}
+
+				g_env->CallVoidMethod(obj, mid, paramObject);
+				it++;
+
+			}
 		}
 
 		virtual void OnRspQryInstrument(CThostFtdcInstrumentField *pInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {
